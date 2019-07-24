27450aa9f640c314722066fe37cf18e67be8681f
clare <chblackburne@gmail.com>
2019-07-24 15:35:01 +0100

    add user info via code generator

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Provider from '../components/Provider';
-import Library from '../components/Library'
+import Library from '../components/Library';
+import UserInfo from '../components/UserInfo';
 
-render(<Provider><Library/></Provider>, document.querySelector('#root'));
+render(
+  <Provider>
+    <UserInfo />
+    <Library/>
+  </Provider>,
+  document.querySelector('#root')
+);
