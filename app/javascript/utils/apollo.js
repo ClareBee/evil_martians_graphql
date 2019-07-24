67f43cd8fe4a641cc5bac3dee31467d1ab753f4e
clare <chblackburne@gmail.com>
2019-07-24 15:42:04 +0100

    add in auth token in apollo.js

--- app/javascript/utils/apollo.js
+++ app/javascript/utils/apollo.js
@@ -25,16 +25,24 @@ export const createClient = (cache, requestLink) => {
     cache,
   });
 };
-// getToken from meta tags to pass forgery protection check
-const getToken = () =>
-  document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-const token = getToken();
-const setTokenForOperation = async operation =>
-  operation.setContext({
+const getTokens = () => {
+  const tokens = {
+    'X-CSRF-Token': document
+      .querySelector('meta[name="csrf-token"]')
+      .getAttribute('content'),
+  };
+  const authToken = localStorage.getItem('mlToken');
+  return authToken ? { ...tokens, Authorization: authToken } : tokens;
+};
+
+const setTokenForOperation = async operation => {
+  return operation.setContext({
     headers: {
-      'X-CSRF-Token': token,
+      ...getTokens(),
     },
   });
+};
+
 // link with token
 const createLinkWithToken = () =>
   new ApolloLink(
