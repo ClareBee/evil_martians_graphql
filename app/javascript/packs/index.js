5120bbe339a28eab47a294753639784841389b1e
clare <chblackburne@gmail.com>
2019-07-24 13:42:46 +0100

    add rspec and factorybot

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -3,4 +3,4 @@ import { render } from 'react-dom';
 import Provider from '../components/Provider';
 import Library from '../components/Library'
 
-render(<Provider>👻</Provider>, document.querySelector('#root'));
+render(<Provider><Library/></Provider>, document.querySelector('#root'));
